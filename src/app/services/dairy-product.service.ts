e34a70c3d0490cb0828a2e544a4ecb20f4b36e6a
MonikaA10 <monikasinghms1014@gmail.com>
2024-09-15 13:02:41 +0530

    first commit

--- src/app/services/dairy-product.service.ts
+++ src/app/services/dairy-product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DairyProduct } from '../models/dairy-product';
-
+ 
 @Injectable({
   providedIn: 'root'
 })
